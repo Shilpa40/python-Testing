8c1025f4cbe7cb0c0bb5a62f4bdb42253156b998
Shilpa <64268141+Shilpa40@users.noreply.github.com>
2023-09-18 16:11:01 +0530

    Update linuxtest1.sh

--- linuxtest1.sh
+++ linuxtest1.sh
@@ -11,3 +11,6 @@ ip=$(ip address| awk '/inet/ {print $2}' | grep -v ^::1 | grep -v ^127)
 #displaying IP addresses
 echo "LIST OF IP ADDRESSES"
 echo "$ip"
+
+# or for ec2
+#internalipaddress=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
