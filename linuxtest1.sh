91b3721e833c65fb8e77c778328716de80afdb70
Shilpa <64268141+Shilpa40@users.noreply.github.com>
2023-09-18 16:12:24 +0530

    Update linuxtest1.sh

--- linuxtest1.sh
+++ linuxtest1.sh
@@ -3,9 +3,8 @@
 ip_address=$(hostname -i)
 echo "LIST OF IP ADDRESS: ${ip_address}"
 
-sed 's/#Rajat/Shilpa/' test.txt
 
-# Extracting lines that match the following pattern
+# Or Extracting lines that match the following pattern
 ip=$(ip address| awk '/inet/ {print $2}' | grep -v ^::1 | grep -v ^127)
 
 #displaying IP addresses
@@ -14,3 +13,6 @@ echo "$ip"
 
 # or for ec2
 #internalipaddress=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Replace something in the file using sed command
+sed 's/#Test/Linux/' test.txt
